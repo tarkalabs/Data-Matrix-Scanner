c69d355c54f4caaf85f7817504997981ccbe3a4d
Ibrahim Hassan <mdibrahimhassan@gmail.com>
2023-03-29 10:28:43 +0530

    Initial Commit

---
+++ DMC-Scanner-AVFoundation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DMC-Scanner-AVFoundation
+//
+//  Created by Ibrahim Hassan on 29/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
